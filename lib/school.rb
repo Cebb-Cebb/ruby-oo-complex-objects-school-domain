fbe8a090eb02f6c35732229b19ca44fda6fe325a
user <github email address>
2020-11-07 15:34:28 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
 # code here!
+require 'pry'
+
+class School
+
+    attr_accessor :roster
+
+    def initialize(name)
+        @roster = {}
+    end
+    
+    def add_student(grade, name)
+        @roster[name] = []
+        @roster[name] << grade 
+        
+         binding.pry 
+        
+
+    end 
+    
+
+
+end 
