f4e2f97c5f7b6612b45bf9cfbd613e01b7ace32e
user <github email address>
2020-11-07 21:33:29 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -3,21 +3,42 @@ require 'pry'
 
 class School
 
-    attr_accessor :roster
+    attr_accessor :roster, :add_student
 
     def initialize(name)
         @roster = {}
+        new_grade = []
+        @name = name 
     end
     
-    def add_student(grade, name)
-        @roster[name] = []
-        @roster[name] << grade 
-        
-         binding.pry 
-        
-
+    def add_student(name, grade)
+      if @roster[grade] 
+        @roster[grade] << name
+        else
+            @roster[grade] = []
+            @roster[grade]  << name 
+        end 
     end 
-    
+      
+   def grade(grade)
+    @roster[grade]
+    # binding.pry 
+   end
 
+   def sort
+    @grade.sort.to_h
+    # @roster.sort {|a ,b| a<=>b  }.to_h
+   end 
 
 end 
+    
+        
+       
+
+        # binding.pry 
+    #     @roster[grade] = []
+    #     @roster[grade] << name 
+    #    elsif @roster.length == 1
+    #     @roster[grade] << name
+    #    else
+       
